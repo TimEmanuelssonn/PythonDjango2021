0b97f9d1517dbbfc8ad36275291530eeb5046ca6
unknown <timemanuelsson1994@gmail.com>
2022-03-31 16:36:24 +0200

    add template and render them out

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('Here are our products')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse('SINGEL PROJECT ' + ' ' + str(pk))
+    return render(request, 'projects/singel-project.html')
