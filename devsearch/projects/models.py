11f30f642d571f5f4e0c78835178d2de3376fca9
unknown <timemanuelsson1994@gmail.com>
2022-04-03 13:30:42 +0200

    add new model to database and adds in to admin site

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    #null=True means that the text could be empty for database and blank for django
+    discription = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    #Make timestamp when created
+    created = models.DateTimeField(auto_now_add=True)
+    #Unique id with uuid, set to pk and not editable
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+
+
