d78e95af26c587ad6a5be8cfd2b5b1ef2e56425a
unknown <timemanuelsson1994@gmail.com>
2022-04-03 20:51:47 +0200

    added model to admin + registered project model in admin

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -12,5 +12,7 @@ class Project(models.Model):
     #Unique id with uuid, set to pk and not editable
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
+    def __str__(self):
+        return self.title
 
 
